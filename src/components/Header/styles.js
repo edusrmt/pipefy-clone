2bad2acc9bff4b3140cd14f92380855500ec25b1
Eduardo Sarmento <edusrmt@ufrn.edu.br>
2020-04-27 15:57:36 -0300

    Implementa o layout base

---
+++ src/components/Header/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 80px;
+  padding: 30px;
+  background-color: #3b5bfd;
+  color: #fff;
+
+  display: flex;
+  align-items: center;
+
+`;
